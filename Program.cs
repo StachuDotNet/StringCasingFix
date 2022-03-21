d12dd6392083c6b1c8085c9bce0540634954fa2c
Stachu Korick <hello@stachu.net>
2022-03-21 08:07:35 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿
+using StringCasingFix;
+
+Console.WriteLine($"Expected: 'TEST'; Actual: '{NetFxUpperCaseInvariant.NetFxToUpperInvariant("test")}");
+Console.WriteLine($"Expected: 'FIFL'; Actual: '{NetFxUpperCaseInvariant.NetFxToUpperInvariant("ﬁﬂ")}");
+Console.WriteLine($"Expected: 'ԵՒ'; Actual: '{NetFxUpperCaseInvariant.NetFxToUpperInvariant("և")}");
